8870d8fb623f819b7db13098364a5ca912c50bbf
Kaur <p_kaur73377@TSS-F1000-MAC11.fc.ca>
2019-12-07 23:05:47 -0500

    added db and scss

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,27 @@ var path = require('path');
 const sql = require('../utils/sql');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  console.log('sent back a static file');
-  res.sendFile((path.join(__dirname, "../views/index.html")));
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Home Page' });
+  // res.sendFile((path.join(__dirname, "../views/index.html")));
 });
 
-// get the dynamic data that goes with each svg graphic
-router.get('/info/:target', (req, res) => {
-  // set up your sql query here and retrieve the relevant data
-  let query = `SELECT * FROM tbl_recycle WHERE id="${req.params.target}"`;
+// get portfolio page
+
+router.get('/portfolio', function (req, res, next) {
+
+  let query = `SELECT * FROM tbl_database`;
 
   sql.query(query, (err, result) => {
-    if (err) { console.log(err); }
+    if (err) { console.log(err); } // something done broke!
 
-    console.log(result);
-    res.json(result[0]);
+    console.log(result); // this should be the database row
+
+    res.render('portfolio', { title: 'Portfolio' , data: result});
   })
-})
+  
+  // res.sendFile((path.join(__dirname, "../views/portfolio.html")));
+});
 module.exports = router;
